b20603ac231385098b20cc4d0566a0fceb986764
Bobby T <bob.tarino@gmail.com>
2021-06-03 15:14:18 -0500

    created connection to MYSQL database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -5,7 +6,18 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// Connect to database // variable db holds coonnection
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Bobcatthedawg10*',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 // Default response for any other request (Not Found) // make sure this is last route
 app.use((req, res) => {
     res.status(404).end();
