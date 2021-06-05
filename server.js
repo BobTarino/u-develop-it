5528d24ff6bdebc0cb7a71fee70d6c98efa3f5f2
Bobby T <bob.tarino@gmail.com>
2021-06-04 23:52:15 -0500

    added route to update candidate's party

--- server.js
+++ server.js
@@ -160,6 +160,35 @@ app.post('/api/candidate', ({ body }, res) => {
     });
   });
 });
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  // forces any PUT request to include a party_id
+  const errors = inputCheck(req.body, 'party_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
 // Create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
 //               VALUES (?,?,?,?)`;
